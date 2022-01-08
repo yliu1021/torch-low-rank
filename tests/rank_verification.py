b2b7009ba6bc8b10742cd10da20f8f2911ac2c21
Yuhan Liu <yliu1021@outlook.com>
2022-01-08 08:49:46 -0800

    added rank verification test

---
+++ tests/rank_verification.py
@@ -0,0 +1,56 @@
+import numpy as np
+from tensorflow.keras import optimizers, losses, metrics
+
+import data
+import model
+import lowrank
+
+
+def get_rank(model):
+    for layer in model.layers:
+        if not isinstance(layer, lowrank.LRDense):
+            continue
+        eff_weight = layer.eff_weight()
+        s = np.linalg.svd(eff_weight, full_matrices=False, compute_uv=False)
+        print(s.min(), s.max(), s.mean())
+        return np.sum(s > 0.001)  # since we compute (U @ V) first, there's fp rounding errors
+
+
+def main():
+    (x_train, y_train), (x_test, y_test) = data.load_data(
+        "cifar100"
+    )
+    lr_model = model.get_lr_conv_model(
+        x_train.shape[1:], y_train.shape[-1], rank=-1
+    )
+    lr_model.compile(
+        optimizer=optimizers.Adam(0.0001),
+        loss=losses.CategoricalCrossentropy(),
+        metrics=[metrics.CategoricalAccuracy()],
+    )
+
+    print(f"Initial rank: {get_rank(lr_model)}")
+    lr_model.fit(
+        x_train,
+        y_train,
+        batch_size=256,
+        epochs=1,
+        validation_data=(x_test, y_test),
+    )
+    print(f"Rank after training: {get_rank(lr_model)}")
+    for layer in lr_model.layers:
+        if not isinstance(layer, lowrank.LRDense):
+            continue
+        layer.set_rank(10)
+    print(f"Rank after dropping: {get_rank(lr_model)}")
+    lr_model.fit(
+        x_train,
+        y_train,
+        batch_size=256,
+        epochs=1,
+        validation_data=(x_test, y_test),
+    )
+    print(f"Final rank: {get_rank(lr_model)}")
+
+if __name__ == "__main__":
+    main()
