0828cdae942ebf4398b4a6a038d8699fb6532a6c
Siddharth Joshi <sjoshi804@gmail.com>
2022-03-15 17:08:05 -0700

    remvoved import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,9 +35,6 @@ release = ''
 #
 # needs_sphinx = '1.0'
 
-# Import Extensions 
-import sphinx_rtd_theme
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
