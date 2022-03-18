de5108bbbe58ba914069e7aea06462823b3792d5
Siddharth Joshi <sjoshi804@gmail.com>
2022-03-17 21:07:41 -0700

    Refactor and documentation update

--- tests/evaluate_sv_ranking.py
+++ tests/evaluate_sv_ranking.py
@@ -1,9 +1,5 @@
 '''
 Evalute Singular Vector Ranking Methods = Looking to determine which method of ranking singular vectors allows us to prune while losing minimal accuracy, highest pruning ration and can be evaluated early in training
-Current Logic: Model - Low Rank Layers with Initial Rank = Max Rank
-- Traing pre-pruning for given number of epochs
-- For each low rank layer, sort singular vectors by method and retain pruning_ratio of the singular vectors
-- Train low rank model for remaining epochs
 '''
 
 import os, sys
@@ -20,18 +16,13 @@ from tests.sort_sv_by_score import SCORING_METHOD, sort_sv_by_score
 from tests import data
 from tests import models
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Evaluate singular vector rankings")
-    parser.add_argument('--method', type=int, help="Method to score ranks by: " + str(list(SCORING_METHOD)))
-    parser.add_argument('--pre_prune_epochs', type=int, help="Number of epochs to train for before pruning")
-    parser.add_argument('--total_epochs', type=int, help="Total number of epochs to train for")
-    parser.add_argument('--fast', action='store_true', default=False)
-    parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument('--pruning_ratio', type=float, help="Ratio of singular vectors pruned from each layer")
-
-    args = parser.parse_args()
-    args.method = SCORING_METHOD(args.method)
-
+def main(args):
+    '''
+    Current Logic (Model - Low Rank Layers with Initial Rank = Max Rank)
+    - Traing pre-pruning for given number of epochs
+    - For each low rank layer, sort singular vectors by method and retain pruning_ratio of the singular vectors
+    - Train low rank model for remaining epochs
+    '''
     gpus = tf.config.list_physical_devices("GPU")
     tf.config.set_visible_devices(gpus[0], "GPU")
 
@@ -73,3 +64,18 @@ if __name__ == "__main__":
     val_metrics = model.evaluate(val_x, val_y, batch_size=args.batch_size)
     for metric, value in zip(model.metrics_names, val_metrics):
         print(f"val_{metric}:", value)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate singular vector rankings")
+    parser.add_argument('--method', type=int, help="Method to score ranks by: " + str(list(SCORING_METHOD)))
+    parser.add_argument('--pre_prune_epochs', type=int, help="Number of epochs to train for before pruning")
+    parser.add_argument('--total_epochs', type=int, help="Total number of epochs to train for")
+    parser.add_argument('--fast', action='store_true', default=False)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--pruning_ratio', type=float, help="Ratio of singular vectors pruned from each layer")
+
+    args = parser.parse_args()
+    args.method = SCORING_METHOD(args.method)
+
+    main(args)
