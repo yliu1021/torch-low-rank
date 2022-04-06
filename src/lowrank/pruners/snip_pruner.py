0d6eda99979d1ae818a263888f949a3dc57042ce
Yuhan Liu <yliu1021@outlook.com>
2022-04-05 21:53:43 -0700

    added batching to snip pruner

--- src/lowrank/pruners/snip_pruner.py
+++ src/lowrank/pruners/snip_pruner.py
@@ -1,6 +1,7 @@
 """
 SNIP SV Pruner - calculates Delta L instead of gradient estimate
 """
+from functools import reduce
 
 import numpy as np
 import tensorflow as tf
@@ -23,20 +24,26 @@ class SnipPruner(AbstractPrunerBase):
         if self.data is None or self.loss is None:
             raise ValueError("Snip pruner requires data and loss function.")
 
-        with tf.GradientTape(watch_accessed_variables=False) as grad_tape:
-            for layer in self.layers_to_prune:
-                # Set mask to all ones to evaluate gradient at $c = 1$
-                self._set_mask_on_layer(
-                    layer, create_mask(layer.rank_capacity, [], inverted=True)
-                )
-                grad_tape.watch(layer._mask)
-                logits = self.model(self.data_x)
-                loss = self.loss(self.data_y, logits)
-        grads = grad_tape.gradient(
-            loss, [layer._mask for layer in self.layers_to_prune]
-        )
-        normalized_grads = SnipPruner.normalize(grads)
-        return normalized_grads
+        final_scores = []
+        for _ in range(16):
+            data_ind = np.random.choice(len(self.data_x), 64, replace=False)
+            data_x = self.data_x[data_ind]
+            data_y = self.data_y[data_ind]
+            with tf.GradientTape(watch_accessed_variables=False) as grad_tape:
+                for layer in self.layers_to_prune:
+                    # Set mask to all ones to evaluate gradient at $c = 1$
+                    self._set_mask_on_layer(
+                        layer, create_mask(layer.rank_capacity, [], inverted=True)
+                    )
+                    grad_tape.watch(layer._mask)
+                    logits = self.model(data_x)
+                    loss = self.loss(data_y, logits)
+            grads = grad_tape.gradient(
+                loss, [layer._mask for layer in self.layers_to_prune]
+            )
+            normalized_grads = SnipPruner.normalize(grads)
+            final_scores.append(normalized_grads)
+        return reduce(lambda x, y: [a + b for a, b in zip(x, y)], final_scores)
 
     @staticmethod
     def normalize(grads):
