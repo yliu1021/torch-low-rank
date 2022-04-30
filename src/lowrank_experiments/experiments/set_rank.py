d7b5c1c43fe75bad5618d4de04a578d87cd5c6c7
Yuhan Liu <yliu1021@outlook.com>
2022-04-30 15:45:08 -0700

    Removed certain data augmentations to match https://github.com/lucaslie/torchprune/tree/main/src/torchprune

--- src/lowrank_experiments/experiments/set_rank.py
+++ src/lowrank_experiments/experiments/set_rank.py
@@ -63,10 +63,10 @@ def main(args):
         samplewise_center=False,  # set each sample mean to 0
         featurewise_std_normalization=False,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        # zca_whitening=False,  # apply ZCA whitening
+        # rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=4,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=4,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
         vertical_flip=False,
     )  # randomly flip images
