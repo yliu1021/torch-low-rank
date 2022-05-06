ba358780d71866256650e016fe6b52df120243bc
Yuhan Liu <yliu1021@outlook.com>
2022-05-06 11:57:39 -0700

    reformatted low_rank_layer.py using black

--- src/lowrank/low_rank_layer.py
+++ src/lowrank/low_rank_layer.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
 from typing import Optional
-import torch 
+import torch
 from torch import TensorType, nn, tensor
 from torch.nn import functional as F
+
 
 class LowRankLayer(nn.Module):
     def __init__(self, layer: nn.Module):
@@ -19,28 +20,34 @@ class LowRankLayer(nn.Module):
             self.dilation = layer.dilation
             self.groups = layer.groups
             self.padding_mode = layer.padding_mode
-            self.layer_type = nn.Conv2d 
+            self.layer_type = nn.Conv2d
         elif isinstance(layer, nn.Linear):
             self.in_features = layer.in_features
             self.out_features = layer.out_features
             self.layer_type = nn.Linear
         else:
             raise ValueError("Unsupported layer provided to LowRankLayer constructor.")
-            
+
         # Copy over weights
         self.original_weights_shape = deepcopy(layer.weight.shape)
         if len(self.original_weights_shape) > 2:
-            self.kernel_w = torch.reshape(layer.weight, (self.in_channels * self.kernel_size[0] * self.kernel_size[1], self.out_channels))
+            self.kernel_w = torch.reshape(
+                layer.weight,
+                (
+                    self.in_channels * self.kernel_size[0] * self.kernel_size[1],
+                    self.out_channels,
+                ),
+            )
         else:
             self.kernel_w = layer.weight
         self.bias = nn.Parameter(layer.bias)
 
         # Initialize other member variables
-        self.kernel_u = None 
+        self.kernel_u = None
         self.kernel_v = None
-        self.kernel_uv = (self.kernel_u, self.kernel_v) 
-        self._mask = None 
-    
+        self.kernel_uv = (self.kernel_u, self.kernel_v)
+        self._mask = None
+
     @property
     def full_rank_mode(self) -> bool:
         """
@@ -88,7 +95,7 @@ class LowRankLayer(nn.Module):
             self.kernel_v = torch.matmul(s_sqrt, v)
         elif len(self._mask.shape) == 2:
             assert new_mask.shape == self.kernel_w.shape, "Invalid shape for mask"
-        
+
         self._mask = new_mask
 
     def forward(self, x):
@@ -97,16 +104,12 @@ class LowRankLayer(nn.Module):
             eff_weights = self.kernel_w
         elif self.weight_masking_mode:
             eff_weights = torch.mul(self.kernel_w, self.mask)
-        else: 
+        else:
             eff_weights = self.kernel_u @ torch.diag(self._mask) @ self.kernel_v
-        
+
         # Do actual forward pass
         if self.layer_type is nn.Linear:
-            return F.linear(
-                input=x,
-                weight=eff_weights,
-                bias=self.bias
-            )
+            return F.linear(input=x, weight=eff_weights, bias=self.bias)
         elif self.layer_type is nn.Conv2d:
             return F.conv2d(
                 input=x,
@@ -115,42 +118,42 @@ class LowRankLayer(nn.Module):
                 stride=self.stride,
                 padding=self.padding,
                 dilation=self.dilation,
-                groups=self.groups
+                groups=self.groups,
             )
         else:
             raise RuntimeError("Layer in bad state, layer type corrupted")
 
 
 if __name__ == "__main__":
-    test_input = tensor([1., 1., 1.])
+    test_input = tensor([1.0, 1.0, 1.0])
 
-    '''
+    """
     Low Rank Linear Test 
-    '''
+    """
     linear = nn.Linear(3, 5)
     lr_linear = LowRankLayer(linear)
 
-    # Sanity check 
-    assert((linear.forward(test_input) == lr_linear.forward(test_input)).all())
+    # Sanity check
+    assert (linear.forward(test_input) == lr_linear.forward(test_input)).all()
 
     # Low Rank Test
-    lr_linear.mask = tensor([1., 1., 0.])
+    lr_linear.mask = tensor([1.0, 1.0, 0.0])
 
     lr_linear.forward(test_input)
 
-    '''
+    """
     Conv2D Test 
-    '''
+    """
 
     conv2d = nn.Conv2d(3, 3, (1, 1), 1)
     lr_conv2d = LowRankLayer(conv2d)
 
-    test_input = tensor([[[1.]], [[1.]], [[1.]]])
-    
-    # Sanity check 
-    assert((lr_conv2d.forward(test_input) == conv2d.forward(test_input)).all())
+    test_input = tensor([[[1.0]], [[1.0]], [[1.0]]])
+
+    # Sanity check
+    assert (lr_conv2d.forward(test_input) == conv2d.forward(test_input)).all()
 
     # Low Rank Test
-    lr_conv2d.mask = tensor([1., 1., 0.])
+    lr_conv2d.mask = tensor([1.0, 1.0, 0.0])
 
     lr_conv2d.forward(test_input)
