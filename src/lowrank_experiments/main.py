ea9f363fa38e7dc763909704993fa661daa2cc87
Yuhan Liu <yliu1021@outlook.com>
2022-05-06 17:09:12 -0700

    added arg parser

--- src/lowrank_experiments/main.py
+++ src/lowrank_experiments/main.py
@@ -1,4 +1,4 @@
-from calendar import c
+import argparse
 
 import data_loader
 from lowrank.pruners import PruningScope
@@ -10,19 +10,32 @@ from torch.optim import lr_scheduler
 
 from lowrank.pruners.alignment_pruner_loss_based import AlignmentPrunerLossBased
 
-def main(preprune_epochs: int, postprune_epochs: int, lr_drops: list[int], device):
+def main(
+    preprune_epochs: int,
+    postprune_epochs: int,
+    lr_drops: list[int],
+    lr: float,
+    momentum: float,
+    weight_decay: float,
+    batch_size: int,
+    device,
+):
     device = torch.device(device)
-    train_dataloader, test_dataloader = data_loader.get_data("cifar10", batch_size=32)
-    model = models.vgg11(batch_norm=False, num_classes=10)
+    train, test = data_loader.get_data("cifar10", batch_size=batch_size)
+    model = models.vgg11(batch_norm=True, num_classes=10)
+    models.convert_model_to_lr(model)
+
     model.to(device=device)
     loss_fn = nn.CrossEntropyLoss()
-    opt = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+    opt = optim.SGD(
+        model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay
+    )
     lr_schedule = lr_scheduler.MultiStepLR(opt, milestones=lr_drops, gamma=0.1)
 
     for epoch in range(preprune_epochs):
         print(f"Pre-prune epoch {epoch}")
-        trainer.train(model, train_dataloader, loss_fn, opt, device=device)
-        trainer.test(model, test_dataloader, loss_fn, device=device)
+        trainer.train(model, train, loss_fn, opt, device=device)
+        trainer.test(model, test, loss_fn, device=device)
         lr_schedule.step()
 
     # Prune
@@ -30,7 +43,7 @@ def main(preprune_epochs: int, postprune_epochs: int, lr_drops: list[int], devic
         model=models.convert_model_to_lr(model),
         scope=PruningScope.GLOBAL,
         sparsity=0.25,
-        dataloader=train_dataloader,
+        dataloader=train,
         loss=loss_fn,
         prune_iterations=1
     )
@@ -42,10 +55,32 @@ def main(preprune_epochs: int, postprune_epochs: int, lr_drops: list[int], devic
 
     for epoch in range(postprune_epochs):
         print(f"Pre-prune epoch {epoch}")
-        trainer.train(model, train_dataloader, loss_fn, opt, device=device)
-        trainer.test(model, test_dataloader, loss_fn, device=device)
+        trainer.train(model, train, loss_fn, opt, device=device)
+        trainer.test(model, test, loss_fn, device=device)
         lr_schedule.step()
 
 
 if __name__ == "__main__":
-    main(preprune_epochs=0, postprune_epochs=1, lr_drops=[10], device='cuda')
+    parser = argparse.ArgumentParser(
+        description="Runs a training session where a model is trained for some epochs, pruned, "
+        "then trained for some more epochs"
+    )
+    parser.add_argument("--preprune_epochs", type=int)
+    parser.add_argument("--postprune_epochs", type=int)
+    parser.add_argument("--lr_drop", type=int, action="append")
+    parser.add_argument("--lr", type=float)
+    parser.add_argument("--momentum", type=float)
+    parser.add_argument("--weight_decay", type=float)
+    parser.add_argument("--batch_size", type=int)
+    parser.add_argument("--device", type=str, default="cpu")
+    args = parser.parse_args()
+    main(
+        preprune_epochs=args.preprune_epochs,
+        postprune_epochs=args.postprune_epochs,
+        lr_drops=sorted(args.lr_drop),
+        lr=args.lr,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+        batch_size=args.batch_size,
+        device=args.device,
+    )
