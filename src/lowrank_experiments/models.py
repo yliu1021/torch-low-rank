dd72c4a3f47cc88ae078e30ea044334029d7f863
Siddharth Joshi <sjoshi804@gmail.com>
2022-05-06 19:48:48 -0700

    bug fixes for convert model

--- src/lowrank_experiments/models.py
+++ src/lowrank_experiments/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from torch import nn
 from torchvision import models
 
@@ -24,21 +25,30 @@ class BasicNet(nn.Module):
         return logits
 
 
-def convert_module_to_lr(module: nn.Module):
+def convert_model_to_lr(model: nn.Module):
     """
     Recursively modifies a module in place to replace instances of conv2d and linear modules into
     low rank alternatives
     :param module: the module to convert
-    :return: the converted module
+    :return:
     """
-    for attr_name in dir(module):
-        attr = getattr(module, attr_name)
-        if isinstance(attr, (nn.Linear, nn.Conv2d)):
-            setattr(module, attr_name, LowRankLayer(attr))
+    if isinstance(model, (nn.Linear, nn.Conv2d)):
+        return LowRankLayer(model)
+
+    for attr_name in dir(model):
+        attr = getattr(model, attr_name)
+        if isinstance(attr, nn.ModuleList) or isinstance(attr, nn.Sequential):
+            for i in range(len(attr)):
+                attr[i] = convert_model_to_lr(attr[i])
+        elif isinstance(attr, nn.ModuleDict):
+            for key in attr.keys():
+                attr[key] = convert_model_to_lr(attr[key])
         elif isinstance(attr, nn.Module):
-            convert_module_to_lr(attr)
-        elif isinstance(attr, list):
-            attr = [convert_module_to_lr(a) for a in attr]
-            setattr(module, attr_name, attr)
+            attr = convert_model_to_lr(attr)
+        else:
+            continue
+        setattr(model, attr_name, attr)
+
+    assert not any([isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear) for layer in model.modules()]), "Convert to lr model failed."
     
-    return module
+    return model
